4041a4963521c4faa6b43310484e5db201062937
ilanolkies <ilan@kinetica-solutions.com>
2017-02-10 14:12:49 -0300

    Numeros Romanos -Pablo

---
+++ Numeros Romanos - Pablo/ClassLibrary1/EnteroARomano.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+
+namespace ClassLibrary1
+{
+
+    #region Tipos
+    enum TipoNumeroRomano
+    {
+        M = 1000,
+        CM = 900,
+        D = 500,
+        CD = 400,
+        C = 100,
+        XC = 90,
+        L = 50,
+        XL = 40,
+        X = 10,
+        IX = 9,
+        V = 5,
+        IV = 4,
+        I = 1
+    }
+
+    public class ParEnteroRomano
+    {
+        public int ValorEntero { get; set; }
+        public string RepresentacionRomana { get; set; }
+
+    } 
+    #endregion
+
+    public class EnteroARomano
+    {
+
+        private List<ParEnteroRomano> _ListaEnteroRomano;
+
+        private void Inicializar()
+        {
+            _ListaEnteroRomano = new List<ParEnteroRomano>()
+            {
+                new ParEnteroRomano()
+                {
+ //...1000
+                    ValorEntero = Convert.ToInt32(TipoNumeroRomano.M),
+                    RepresentacionRomana = TipoNumeroRomano.M.ToString()
+
+                },
+
+                new ParEnteroRomano()
+                {
+ //...900
+                    ValorEntero = Convert.ToInt32(TipoNumeroRomano.CM),
+                    RepresentacionRomana = TipoNumeroRomano.CM.ToString()
+
+                },
+
+                new ParEnteroRomano()
+                {
+ //...500
+                    ValorEntero = Convert.ToInt32(TipoNumeroRomano.D),
+                    RepresentacionRomana = TipoNumeroRomano.D.ToString()
+
+                },
+
+                new ParEnteroRomano()
+                {
+ //...400           
+                    ValorEntero = Convert.ToInt32(TipoNumeroRomano.CD),
+                    RepresentacionRomana = TipoNumeroRomano.CD.ToString()
+
+                },
+
+                new ParEnteroRomano()
+                {
+ //...100
+                    ValorEntero = Convert.ToInt32(TipoNumeroRomano.C),
+                    RepresentacionRomana = TipoNumeroRomano.C.ToString()
+
+                },
+
+                new ParEnteroRomano()
+                {
+ //...90
+                    ValorEntero = Convert.ToInt32(TipoNumeroRomano.XC),
+                    RepresentacionRomana = TipoNumeroRomano.XC.ToString()
+
+                },
+
+                new ParEnteroRomano()
+                {
+ //...50
+                    ValorEntero = Convert.ToInt32(TipoNumeroRomano.L),
+                    RepresentacionRomana = TipoNumeroRomano.L.ToString()
+
+                },
+
+                new ParEnteroRomano()
+                {
+ //...40
+                    ValorEntero = Convert.ToInt32(TipoNumeroRomano.XL),
+                    RepresentacionRomana = TipoNumeroRomano.XL.ToString()
+
+
+                },
+
+                new ParEnteroRomano()
+                {
+ //...10
+                    ValorEntero = Convert.ToInt32(TipoNumeroRomano.X),
+                    RepresentacionRomana = TipoNumeroRomano.X.ToString()
+
+                },
+
+                new ParEnteroRomano()
+                {
+ //...9
+                    ValorEntero = Convert.ToInt32(TipoNumeroRomano.IX),
+                    RepresentacionRomana = TipoNumeroRomano.IX.ToString()
+
+                },
+
+                new ParEnteroRomano()
+                {
+ //...5
+                    ValorEntero = Convert.ToInt32(TipoNumeroRomano.V),
+                    RepresentacionRomana = TipoNumeroRomano.V.ToString()
+
+                },
+
+                new ParEnteroRomano()
+                {
+ //...4
+                    ValorEntero = Convert.ToInt32(TipoNumeroRomano.IV),
+                    RepresentacionRomana = TipoNumeroRomano.IV.ToString()
+
+                },
+
+                new ParEnteroRomano()
+                {
+ //...1
+                    ValorEntero = Convert.ToInt32(TipoNumeroRomano.I),
+                    RepresentacionRomana = TipoNumeroRomano.I.ToString()
+
+                }
+
+
+            };
+
+
+        }
+
+        public EnteroARomano()
+        {
+            Inicializar();
+        }
+
+        public string ConvertirEnteroARomano(int numero)
+        {
+            if (numero < 1 || numero > 3000)
+            {
+                throw new IndexOutOfRangeException("El numero se encuentra fuera del rango esperado(1 - 3000).");
+            }
+
+            var builder = new StringBuilder();
+
+            foreach (var par in _ListaEnteroRomano)
+            {
+                while (numero >= par.ValorEntero)
+                {
+                    builder.Append(par.RepresentacionRomana);
+                    numero -= par.ValorEntero;
+                }
+
+            }
+
+            return builder.ToString();
+        }
+
+        
+    }
+}
---
+++ Numeros Romanos - Pablo/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+
+namespace ClassLibrary1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SumarDosEnteros()
+        {
+            Calculadora calc = new Calculadora();
+
+            var resultado = calc.Sumar(1,2);
+
+            var resultadoEsperado = 3;
+
+            Assert.AreEqual(resultado, resultadoEsperado);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void NumeroMayorATresMilGeneraExcepcionFueraDeRango()
+        {
+            var converter = new EnteroARomano();
+            converter.ConvertirEnteroARomano(3001);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void NumeroMenorAUnoGeneraExcepcionFueraDeRango()
+        {
+           var converter = new EnteroARomano();
+           converter.ConvertirEnteroARomano(-1); 
+        
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void NumeroCeroGeneraExcepcionFueraDeRango()
+        {
+            var converter = new EnteroARomano();
+            converter.ConvertirEnteroARomano(0);
+
+        }
+
+        [TestMethod]
+        public void NumeroUnoResultadoEsperadoI()
+        {
+            var converter = new EnteroARomano();
+            var result = converter.ConvertirEnteroARomano(1);
+
+            Assert.AreEqual(result, "I");
+
+        }
+
+        [TestMethod]
+        public void NumeroTresMilResultadoEsperadoMMM()
+        {
+            var converter = new EnteroARomano();
+            var result = converter.ConvertirEnteroARomano(3000);
+
+            Assert.AreEqual(result, "MMM");
+
+        }
+
+        [TestMethod]
+        public void Numero55ResultadoEsperadoLV()
+        {
+            var converter = new EnteroARomano();
+            var result = converter.ConvertirEnteroARomano(55);
+
+            Assert.AreEqual(result, "LV");
+
+        }
+
+        [TestMethod]
+        public void NumeroCienResultadoEsperadoC()
+        {
+            var converter = new EnteroARomano();
+            var result = converter.ConvertirEnteroARomano(100);
+
+            Assert.AreEqual(result, "C");
+
+        }
+
+        [TestMethod]
+        public void Numero500ResultadoEsperadoD()
+        {
+            var converter = new EnteroARomano();
+            var result = converter.ConvertirEnteroARomano(500);
+
+            Assert.AreEqual(result, "D");
+
+        }
+
+        [TestMethod]
+        public void Numero599ResultadoEsperadoDXCIX()
+        {
+            var converter = new EnteroARomano();
+            var result = converter.ConvertirEnteroARomano(599);
+
+            Assert.AreEqual(result, "DXCIX");
+
+        }
+
+        [TestMethod]
+        public void Numero2013ResultadoEsperadoMMXIII()
+        {
+            var converter = new EnteroARomano();
+            var result = converter.ConvertirEnteroARomano(2013);
+
+            Assert.AreEqual(result, "MMXIII");
+
+        }
+
+        [TestMethod]
+        public void Numero2347ResultadoEsperadoMMCCCXLVII()
+        {
+            var converter = new EnteroARomano();
+            var result = converter.ConvertirEnteroARomano(2347);
+
+            Assert.AreEqual(result, "MMCCCXLVII");
+
+        }
+    }
+}
