36769fd4a7f3f60e37e3f415f6f92db24f097907
pabloeeidelman <pabloe@kinetica-solutions.com>
2017-02-14 16:31:03 -0300

    Commit inicial

---
+++ KataAddStrings - Pablo/KataAddStrings/Calculadora.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataAddStrings
+{
+    public class Calculadora
+    {
+        int num = 0;
+        
+
+        public int Add(string numeros)
+        {
+            int length = numeros.Length;
+            char[] array = new char[length];
+            array = numeros.ToCharArray();
+            char del = ',';
+                       
+            if (numeros == "")
+            {                
+                num = 0;
+                return num;
+            }            
+
+            if (array.Length == 1)
+            {
+                num = Convert.ToInt32(numeros);
+                return num;
+            }
+
+            for (int i = 0; i < length; i++)
+            {
+                if (array[i] == '/')
+                {
+                    del = array[2];
+                    i += 3;
+                }
+
+                if (array[i] != ',' && array[i] != del)
+                {
+                    num += array[i].ConvertirAEntero();
+                }                           
+            }
+
+            return num;
+
+           
+
+
+
+
+        }
+    }
+}
---
+++ KataAddStrings - Pablo/KataAddStrings/ExtensionMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataAddStrings
+{
+    public static class ExtensionMethods
+    {
+        public static int ConvertirAEntero(this char x)
+        {
+            int a;
+
+            a = (int)char.GetNumericValue(x);
+
+            if (a < 0)
+            {
+                throw new Exception("No se permiten números negativos");
+            }
+
+            return a;
+
+        }
+    }
+}
